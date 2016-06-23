93df8020c1fa13c70bdb1ec45e76d648ed9138df
nishikawa_naoto <s1210202@gmail.com>
2016-06-24 07:37:59 +0900

    fix header url

--- js/firstImpression.js
+++ js/firstImpression.js
@@ -1,4 +1,4 @@
 $(function() {
-	    $("#header").load("http://naoto24kawa.github.io/navigator.html");
+	    $("#header").load("http://naoto24kawa.github.io/html/navigator.html");
 	    // $("#footer").load("./files/to/footer.html");
 	});
